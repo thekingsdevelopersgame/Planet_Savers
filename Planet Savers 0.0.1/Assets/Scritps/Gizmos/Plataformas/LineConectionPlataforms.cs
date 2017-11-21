44957e9fca3caf7f8d19c05333dd8f0560b7d1b5
TheKingOfShadows98 <thekingofshadows98@gmail.com>
2017-11-21 00:25:20 -0600

    Plataformas Moviles!
    Ahora Se pueden Usar plataformas Pequenas moviles para mas Diversion y dificultad

---
+++ Planet Savers 0.0.1/Assets/Scritps/Gizmos/Plataformas/LineConectionPlataforms.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineConectionPlataforms : MonoBehaviour {
+
+   
+    public Transform startT;
+    public Transform endT;
+
+	
+	void OnDrawGizmosSelected ()
+    {
+      
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(startT.position , endT.position);
+    }
+}
---
+++ Planet Savers 0.0.1/Assets/Scritps/Plataformas/PlatMovAnyPos.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatMovAnyPos : MonoBehaviour {
+    // Nuesta Plataforma
+    public Transform plataforma;
+    // Puntos De paso De la Plataforma
+    public Transform[] Posisiones;
+    // Velocidad de la Plataforma
+    public float Velocity;
+
+    // Indicador de La Siguiente plataforma
+    public int next_point = 0;
+	
+
+	void Start () {
+        // Setear Plataforma en la Posicion 1
+        if (plataforma != null && Posisiones != null) {
+            foreach (Transform a in Posisiones) {
+                a.parent = null;
+            }
+            plataforma.position = Posisiones[0].position;
+            if (Posisiones.Length - 1  >= next_point + 1) { 
+            next_point += 1;
+            }
+        }
+	}
+
+
+    void FixedUpdate()
+    {
+        // Cambiar de Posicion Si ah llegado al destino
+        if (plataforma.position == Posisiones[next_point].position) {
+            if (Posisiones.Length - 1 >= next_point + 1)
+            {
+                next_point += 1;
+            } else if(Posisiones.Length -1 == next_point) {
+                next_point = 0;
+            }
+        }
+        // Desplazamiento
+        if (plataforma != null && Posisiones != null)
+        {
+           plataforma.position = Vector3.MoveTowards(plataforma.position, Posisiones[next_point].position, Velocity * Time.deltaTime);
+
+        }
+    }
+}
